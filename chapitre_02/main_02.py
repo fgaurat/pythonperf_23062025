de546c6ed85d0eaad93fe07abac5d183c9677ee8
Frédéric GAURAT <fgaurat@gmail.com>
2025-06-23 17:03:50 +0200

    add decorateur

--- chapitre_02/main_02.py
+++ chapitre_02/main_02.py
@@ -1,5 +1,45 @@
+
+
+
+# def call_say_hello(name:str)->str:
+#     print("in",name)
+#     s = say_hello(name)
+#     print("out",s)
+#     return s
+
+
+def do_log(func):
+
+    def wrapper(*args,**kwargs):
+        print("in",args,kwargs)
+        r = func(*args,**kwargs)
+        print("out",r)
+        return r
+    
+    return wrapper
+
+def do_secu(func):
+
+    def wrapper(*args,**kwargs):
+        print("the secu",args,kwargs)
+        r = func(*args,**kwargs)
+        return r
+    
+    return wrapper
+
+
+    
+@do_log
+def say_hello(name:str)->str:
+    """
+    Dire Bonjour à name
+    """
+    s = f"Bonjour {name}"
+    return s
+
 def main():
-    pass
+    r = say_hello('Fred')
+    print(r)
 
 if __name__ == '__main__':
     main()
