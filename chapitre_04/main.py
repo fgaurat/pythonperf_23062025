7f9f947bfeb5673342b79b2fd5e15de4b1c5deee
Frédéric GAURAT <fgaurat@gmail.com>
2025-06-24 13:51:42 +0200

    fix

--- chapitre_04/main.py
+++ chapitre_04/main.py
@@ -1,10 +1,15 @@
 from Carre import Carre
+from Cercle import Cercle
 
 def main():
     c = Carre(3)
     print(c.surface)
     print(c.longueur)
     print(c.largeur)
+
+    ce = Cercle(3)
+    print(f"{ce.rayon=}")
+    print(f"{ce.surface=}")
 
 if __name__ == '__main__':
     main()
