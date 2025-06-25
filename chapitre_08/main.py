424b6f39e6168310d19fcc6cce3ad9e2ff223985
Frédéric GAURAT <fgaurat@gmail.com>
2025-06-25 09:48:58 +0200

    start DAO

--- chapitre_08/main.py
+++ chapitre_08/main.py
@@ -1,16 +1,12 @@
-import csv
-import sqlite3
+
+from CustomerDAO import CustomerDAO
 
 def main():
-    with sqlite3.connect("customers.db") as con:
-        cur = con.cursor()
-        sql_insert="INSERT INTO customers_tbl(first_name,last_name,email,gender,ip_address) VALUES(?,?,?,?,?)"
-        # context manager
-        with open('MOCK_DATA.csv', newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                del row['id']
-                cur.execute(sql_insert,list(row.values()))
+    dao = CustomerDAO("customers.db")
+    customers = dao.find_all()
+
+    for customer in customers:
+        print(customer)
 
 if __name__ == '__main__':
     main()
