2cb692648af2a42167c58e5584b7a197c14b88e5
Frédéric GAURAT <fgaurat@gmail.com>
2025-06-25 12:31:49 +0200

    download log files

--- chapitre_09/main.py
+++ chapitre_09/main.py
@@ -1,16 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
+import warnings
+warnings.filterwarnings('ignore')  # Suppress all warnings
 
 def main():
     url = "https://logs.eolem.com/"
-    response = requests.get(url)
+    response = requests.get(url,verify=False)
 
     # print(response.text)
     soup = BeautifulSoup(response.text, 'html.parser')
     all_a = [a['href'] for a in soup.find_all('a') if a['href'].endswith('.log')]
 
-    pprint(all_a)
+
+    for log_file in all_a:
+        url_log_file = f"{url}{log_file}"
+        response = requests.get(url_log_file)
+        with open(log_file,"w") as f:
+            f.write(response.text)
+
 
 
 
